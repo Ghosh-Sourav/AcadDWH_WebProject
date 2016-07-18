17363559f8f128fec3d2c3141e82e9b00b584426
Sourav Ghosh <sourav.ghosh.840001@gmail.com>
2016-07-19 00:11:24 +0530

    SCP code bugs fixed

--- AcadDWH_WebProject/src/in/ac/iitkgp/acaddwh/util/SCP.java
+++ AcadDWH_WebProject/src/in/ac/iitkgp/acaddwh/util/SCP.java
@@ -9,27 +9,28 @@ import com.jcraft.jsch.Session;
 
 public class SCP {
 
-	public static void main() {
+	public static void main(String[] args) {
 		try {
 			JSch jsch = new JSch();
 
 			Session session = jsch.getSession("15CS60R16", "10.5.30.101", 22);
-			session.setPassword("**********");
+			session.setPassword("");
 			session.setConfig("StrictHostKeyChecking", "no");
 			session.connect();
 
 			ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
 			channel.connect();
 
-			File localFile = new File("localfilepath");
+			File localFile = new File("/home/sourav/AcadDWH/AcadDWH_Data/20160718035530444_IITKGP_dim_departments-hive.csv");
 			// If you want you can change the directory using the following
 			// line.
 			
-			channel.cd("AcadDWH");
+			channel.cd("/home/mtech/15CS60R16/AcadDWH_Data");
 			channel.put(new FileInputStream(localFile), localFile.getName());
 			
 			channel.disconnect();
 			session.disconnect();
+			System.out.println("Sent");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
